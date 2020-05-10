5c7615cbeafbe803820a1620dd46e7958b0b23db
grozzzny <grozzzny@list.ru>
2020-05-10 12:36:48 +0200

    add sections

---
+++ views/site/sections/_presentation.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\web\View;
+
+/**
+ * @var View $this
+ */
+
+?>
+
+
+<div class="feature-big">
+    <div class="container">
+
+        <div class="mt-5 row mb-5 site-section ">
+            <div class="col-lg-7 order-1 order-lg-2" data-aos="fade-left">
+                <img src="images/undraw_metrics_gtu7.svg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-5 pr-lg-5 mr-auto mt-5 order-2 order-lg-1">
+                <h2 class="text-black">Communicate and gather feedback</h2>
+                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem neque nisi architecto autem molestias corrupti officia veniam</p>
+
+
+
+                <div class="author-box" data-aos="fade-right">
+                    <div class="d-flex mb-4">
+                        <div class="mr-3">
+                            <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded-circle">
+                        </div>
+                        <div class="mr-auto text-black">
+                            <strong class="font-weight-bold mb-0">Kimberly Gush</strong> <br>
+                            Co-Founder, XYZ Inc.
+                        </div>
+                    </div>
+                    <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vitae ipsa asperiores inventore aperiam iure?&rdquo;</blockquote>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
