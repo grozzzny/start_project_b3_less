4763eb02b9d76c2250fe49f2221ef2451749b361
grozzzny <grozzzny@list.ru>
2020-05-28 13:45:47 +0200

    ended features section

--- views/site/sections/_features.php
+++ views/site/sections/_features.php
@@ -1,5 +1,6 @@
 <?php
 
+use grozzzny\admin\modules\features\models\AdminFeatures;
 use grozzzny\admin\modules\text\LiveEditText;
 use yii\web\View;
 
@@ -7,6 +8,9 @@ use yii\web\View;
  * @var View $this
  */
 
+$models = AdminFeatures::find()->andWhere(['active' => true])->orderBy(['position' => SORT_ASC])->all();
+
+if(count($models) == 0) return;
 ?>
 
 
@@ -19,88 +23,20 @@ use yii\web\View;
             </div>
         </div>
         <div class="row align-items-stretch">
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+            <? foreach ($models as $i => $model): ?>
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up" data-aos-delay="<?=$i*100?>">
 
                 <div class="unit-4 d-block">
                     <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-autorenew"></span></span>
+                        <span class="icon-wrap"><span class="text-primary <?=$model->icon?>"></span></span>
                     </div>
                     <div>
-                        <h3>Marketing Consulting</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up" data-aos-delay="100">
-
-                <div class="unit-4 d-block">
-                    <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-store_mall_directory"></span></span>
-                    </div>
-                    <div>
-                        <h3>Market Analysis</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
+                        <h3><?=$model->liveEditTitle?></h3>
+                        <p><?=$model->description?></p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up"  data-aos-delay="200">
-                <div class="unit-4 d-block">
-                    <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-shopping_basket"></span></span>
-                    </div>
-                    <div>
-                        <h3>Easy Purchase</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
-                <div class="unit-4 d-block">
-                    <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-settings_backup_restore"></span></span>
-                    </div>
-                    <div>
-                        <h3>Free Updates</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up" data-aos-delay="100">
-                <div class="unit-4 d-block">
-                    <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-sentiment_satisfied"></span></span>
-                    </div>
-                    <div>
-                        <h3>100% Satistified</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
-                    </div>
-                </div>
-
-
-            </div>
-
-            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up" data-aos-delay="200">
-                <div class="unit-4 d-block">
-                    <div class="unit-4-icon mb-3">
-                        <span class="icon-wrap"><span class="text-primary icon-power"></span></span>
-                    </div>
-                    <div>
-                        <h3>Easy Plugin</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
-                        <p><a href="#">Learn More</a></p>
-                    </div>
-                </div>
-            </div>
-
+            <? endforeach;?>
         </div>
     </div>
 </div>
