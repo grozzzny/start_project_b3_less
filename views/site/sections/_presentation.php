a01934f14497e1ad6e962a00ffc82b521d469736
grozzzny <grozzzny@list.ru>
2020-05-28 16:25:23 +0200

    fix section presentation

--- views/site/sections/_presentation.php
+++ views/site/sections/_presentation.php
@@ -1,5 +1,6 @@
 <?php
 
+use grozzzny\admin\modules\text\LiveEditText;
 use yii\web\View;
 
 /**
@@ -14,11 +15,11 @@ use yii\web\View;
 
         <div class="mt-5 row mb-5 site-section ">
             <div class="col-lg-7 order-1 order-lg-2" data-aos="fade-left">
-                <img src="images/undraw_metrics_gtu7.svg" alt="Image" class="img-fluid">
+                <img src="/images/undraw_metrics_gtu7.svg" alt="Image" class="img-fluid">
             </div>
             <div class="col-lg-5 pr-lg-5 mr-auto mt-5 order-2 order-lg-1">
-                <h2 class="text-black">Communicate and gather feedback</h2>
-                <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem neque nisi architecto autem molestias corrupti officia veniam</p>
+                <h2 class="text-black"><?=LiveEditText::widget(['slug' => 'section-presentation-heading', 'label' => Yii::t('app', 'Communicate and gather feedback')])?></h2>
+                <p class="mb-4"><?=LiveEditText::widget(['slug' => 'section-presentation-heading', 'label' => Yii::t('app', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem neque nisi architecto autem molestias corrupti officia veniam')])?></p>
 
 
 
@@ -28,11 +29,13 @@ use yii\web\View;
                             <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded-circle">
                         </div>
                         <div class="mr-auto text-black">
-                            <strong class="font-weight-bold mb-0">Kimberly Gush</strong> <br>
-                            Co-Founder, XYZ Inc.
+                            <strong class="font-weight-bold mb-0"><?=LiveEditText::widget(['slug' => 'section-presentation-name', 'label' => Yii::t('app', 'Kimberly Gush')])?></strong> <br>
+                            <?=LiveEditText::widget(['slug' => 'section-presentation-location', 'label' => Yii::t('app', 'Co-Founder, XYZ Inc.')])?>
                         </div>
                     </div>
-                    <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vitae ipsa asperiores inventore aperiam iure?&rdquo;</blockquote>
+                    <blockquote>
+                        <?=LiveEditText::widget(['slug' => 'section-presentation-quote', 'label' => Yii::t('app', '&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus vitae ipsa asperiores inventore aperiam iure?&rdquo;')])?>
+                    </blockquote>
                 </div>
             </div>
         </div>
