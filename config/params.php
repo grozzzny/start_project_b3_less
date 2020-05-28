0986241b952b6427cb6f84ab7a9d03fc79c2de2a
grozzzny <grozzzny@list.ru>
2020-05-28 18:44:48 +0200

    fix section Testimonials

--- config/params.php
+++ config/params.php
@@ -4,4 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'noimage' => '/images/noimage.jpg',
+    'nophoto' => '/images/nophoto.png',
 ];
--- views/site/sections/_testimonial.php
+++ views/site/sections/_testimonial.php
@@ -1,11 +1,16 @@
 <?php
 
+use grozzzny\admin\modules\testimonials\models\AdminTestimonials;
+use grozzzny\admin\modules\text\LiveEditText;
 use yii\web\View;
 
 /**
  * @var View $this
  */
 
+$models = AdminTestimonials::find()->andWhere(['active' => true])->orderBy(['position' => SORT_ASC])->all();
+
+if(count($models) == 0) return;
 ?>
 
 
@@ -13,70 +18,23 @@ use yii\web\View;
     <div class="container">
         <div class="row mb-5">
             <div class="col-12 text-center">
-                <h2 class="section-title mb-3">Testimonials</h2>
+                <h2 class="section-title mb-3"><?=LiveEditText::widget(['slug' => 'section-testimonial-heading', 'label' => Yii::t('app', 'Testimonials')])?></h2>
             </div>
         </div>
     </div>
     <div class="slide-one-item home-slider owl-carousel">
+        <? foreach ($models as $model):?>
         <div>
             <div class="testimonial">
                 <figure class="mb-4 d-block align-items-center justify-content-center">
-                    <div><img src="images/person_3.jpg" alt="Image" class="w-100 img-fluid mb-3 shadow"></div>
+                    <div><img src="<?= $model->getImage(500, 500) ?>" alt="Image" class="w-100 img-fluid mb-3 shadow"></div>
                 </figure>
                 <blockquote class="mb-3">
-                    <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
+                    <p><?= $model->liveEditDescription ?></p>
                 </blockquote>
-                <p class="text-black"><strong>John Smith</strong></p>
-
-
+                <p class="text-black"><strong><?= $model->name ?></strong></p>
             </div>
         </div>
-        <div>
-            <div class="testimonial">
-
-                <figure class="mb-4 d-block align-items-center justify-content-center">
-                    <div><img src="images/person_2.jpg" alt="Image" class="w-100 img-fluid mb-3 shadow"></div>
-                </figure>
-
-                <blockquote class="mb-3">
-                    <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-                </blockquote>
-
-                <p class="text-black"><strong>Robert Aguilar</strong></p>
-
-
-            </div>
-        </div>
-
-        <div>
-            <div class="testimonial">
-                <figure class="mb-4 d-block align-items-center justify-content-center">
-                    <div><img src="images/person_4.jpg" alt="Image" class="w-100 img-fluid mb-3 shadow"></div>
-                </figure>
-                <blockquote class="mb-3">
-                    <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-                </blockquote>
-                <p class="text-black"><strong>Roger Spears</strong></p>
-
-
-            </div>
-
-        </div>
-
-        <div>
-            <div class="testimonial">
-                <figure class="mb-4 d-block align-items-center justify-content-center">
-                    <div><img src="images/person_1.jpg" alt="Image" class="w-100 img-fluid mb-3 shadow"></div>
-                </figure>
-                <blockquote class="mb-3">
-                    <p>&ldquo;Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur unde reprehenderit aperiam quaerat fugiat repudiandae explicabo animi minima fuga beatae illum eligendi incidunt consequatur. Amet dolores excepturi earum unde iusto.&rdquo;</p>
-                </blockquote>
-                <p class="text-black"><strong>Kyle McDonald</strong></p>
-
-
-            </div>
-
-        </div>
-
+        <? endforeach;?>
     </div>
 </div>
