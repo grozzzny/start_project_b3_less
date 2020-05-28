2da988a68103f6488493109c9eb3196418297ca6
grozzzny <grozzzny@list.ru>
2020-05-28 16:30:19 +0200

    fix section about

--- views/site/sections/_about.php
+++ views/site/sections/_about.php
@@ -1,5 +1,6 @@
 <?php
 
+use grozzzny\admin\modules\text\LiveEditText;
 use yii\web\View;
 
 /**
@@ -13,7 +14,7 @@ use yii\web\View;
     <div class="container">
         <div class="row mb-5">
             <div class="col-12 text-center">
-                <h2 class="section-title mb-3">About Us</h2>
+                <h2 class="section-title mb-3"><?=LiveEditText::widget(['slug' => 'section-about-heading', 'label' => Yii::t('app', 'About Us')])?></h2>
             </div>
         </div>
         <div class="row mb-5">
@@ -21,14 +22,13 @@ use yii\web\View;
                 <img src="images/undraw_bookmarks_r6up.svg" alt="Image" class="img-fluid">
             </div>
             <div class="col-lg-5 ml-auto pl-lg-5">
-                <h2 class="text-black mb-4 h3 font-weight-bold">Our Mission</h2>
-                <p class="mb-4">Eos cumque optio dolores excepturi rerum temporibus magni recusandae eveniet, totam omnis consectetur maxime quibusdam expedita dolorem dolor nobis dicta labore quaerat esse magnam unde, aperiam delectus! At maiores, itaque.</p>
+                <h2 class="text-black mb-4 h3 font-weight-bold"><?=LiveEditText::widget(['slug' => 'section-about-title', 'label' => Yii::t('app', 'Our Mission')])?></h2>
+                <p class="mb-4"><?=LiveEditText::widget(['slug' => 'section-about-description', 'label' => Yii::t('app', 'Eos cumque optio dolores excepturi rerum temporibus magni recusandae eveniet, totam omnis consectetur maxime quibusdam expedita dolorem dolor nobis dicta labore quaerat esse magnam unde, aperiam delectus! At maiores, itaque.')])?></p>
                 <ul class="ul-check mb-5 list-unstyled success">
-                    <li>Laborum enim quasi at modi</li>
-                    <li>Ad at tempore</li>
-                    <li>Labore quaerat esse</li>
+                    <li><?=LiveEditText::widget(['slug' => 'section-about-featur-1', 'label' => Yii::t('app', 'Laborum enim quasi at modi')])?></li>
+                    <li><?=LiveEditText::widget(['slug' => 'section-about-featur-2', 'label' => Yii::t('app', 'Ad at tempore')])?></li>
+                    <li><?=LiveEditText::widget(['slug' => 'section-about-featur-3', 'label' => Yii::t('app', 'Labore quaerat esse')])?></li>
                 </ul>
-                <p><a href="#" class="btn btn-primary">Learn More</a></p>
             </div>
         </div>
 
