3b9d928b92599d5312736c94ec5a1f431d67af80
grozzzny <1234578>
2020-06-19 20:27:23 +0200

    add statistics

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -100,7 +100,27 @@ use yii\web\View;
             <a href="/"> STICK-RACING </a>
             by
             <a href="https://pr-kenig.ru"> PR-kenig </a>
+            <!-- Yandex.Metrika informer -->
+            <a href="https://metrika.yandex.ru/stat/?id=65018113&amp;from=informer"
+               target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/65018113/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                                   style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="65018113" data-lang="ru" /></a>
+            <!-- /Yandex.Metrika informer -->
 
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript" >
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(65018113, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/65018113" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
         </div>
     </div>
     <!--/.Copyright-->
