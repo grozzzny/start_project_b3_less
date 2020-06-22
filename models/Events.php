7e123f43c1f9ae62692a7d68e81f0d1f7b388e62
grozzzny <grozzzny@list.ru>
2020-06-22 23:47:00 +0200

    fix settings page

--- models/Events.php
+++ models/Events.php
@@ -238,11 +238,19 @@ class Events extends \yii\db\ActiveRecord
     {
         if(!$this->isActive) return false;
 
+        var_dump('getIsOpenRegistration');
+        var_dump(date('d.m.Y H:i', $this->getOldAttribute('time_from')));
+        var_dump(date('d.m.Y H:i', time()));
+
         return $this->getOldAttribute('time_from') >= time();
     }
 
     public function getIsEndedEvent()
     {
+        var_dump('getIsEndedEvent');
+        var_dump(date('d.m.Y H:i', $this->getOldAttribute('time_to')));
+        var_dump(date('d.m.Y H:i', time()));
+
         return $this->getOldAttribute('time_to') <= time();
     }
 
