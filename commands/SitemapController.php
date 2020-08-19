bc5ae2b9b777fe8d32304d2931e9a6e81cff2e8f
grozzzny <grozzzny@list.ru>
2020-08-19 20:37:52 +0200

    add sitemap

--- commands/SitemapController.php
+++ commands/SitemapController.php
@@ -1,10 +1,11 @@
 <?php
 namespace app\commands;
 
+use app\models\AdminArticles;
 use app\models\Category;
 use app\models\Item;
+use grozzzny\sitemap\components\Sitemap;
 use grozzzny\sitemap\controllers\ConsoleController;
-use grozzzny\sitemap\models\Sitemap;
 
 /**
  * Console
@@ -14,29 +15,99 @@ use grozzzny\sitemap\models\Sitemap;
  *
  * crontab -e
  * 0 5 * * * /opt/php7.0/bin/php /home/c/cd51932/public_html/yii sitemap/console/update
- *
- * After save
- * WebConsole::console()->runAction('sitemap/console/update');
  */
 class SitemapController extends ConsoleController
 {
+    public $lastmodStaticPage = '2020-08-19';
 
-    public function getDataSitemap()
+    public $staticPages = [
+        [
+            'loc' => '/',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_100,
+        ],
+        [
+            'loc' => '/catalog',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_60,
+        ],
+        [
+            'loc' => '/service',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_60,
+        ],
+        [
+            'loc' => '/about',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_60,
+        ],
+        [
+            'loc' => '/delivery',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_60,
+        ],
+        [
+            'loc' => '/contacts',
+            'lastmod' => '2020-08-19',
+            'changefreq' => Sitemap::CHANGEFREQ_MONTHLY,
+            'priority' => Sitemap::PRIORITY_60,
+        ],
+    ];
+
+    protected function dataSitemap()
     {
-//        $this->getDataCatalog();
+        $this->generateArticles();
+        $this->generateCategories();
+        $this->generateItems();
     }
 
-    public function getDataCatalog()
+    protected function generateArticles()
     {
-        $items = Item::find()
-            ->statusOn()
-            ->category(Category::findOne(['slug' => 'drova']))
+        $models = AdminArticles::find()
+            ->andWhere(['active' => true])
             ->all();
 
-        foreach($items as $item){
-            $this->data_sitemap['drova'][] = array(
-                'loc'           => $this->module->domain . '/firewood/view/' . $item->slug,
-                'lastmod'       => Sitemap::lastmodFormat($item->updated_at),
+        foreach($models as $model){
+            $this->data_sitemap['articles'][] = array(
+                'loc'           => $model->link,
+                'lastmod'       => Sitemap::lastmodFormat($model->updated_at),
+                'changefreq'    => Sitemap::CHANGEFREQ_MONTHLY,
+                'priority'      => Sitemap::PRIORITY_60,
+            );
+        }
+    }
+
+    protected function generateCategories()
+    {
+        $models = Category::find()
+            ->andWhere(['status' => true])
+            ->all();
+
+        foreach($models as $model){
+            $this->data_sitemap['categories'][] = array(
+                'loc'           => $model->link,
+                'lastmod'       => Sitemap::lastmodFormat($model->updated_at),
+                'changefreq'    => Sitemap::CHANGEFREQ_MONTHLY,
+                'priority'      => Sitemap::PRIORITY_60,
+            );
+        }
+    }
+
+    protected function generateItems()
+    {
+        $models = Item::find()
+            ->andWhere(['status' => true])
+            ->all();
+
+        foreach($models as $model){
+            $this->data_sitemap['items'][] = array(
+                'loc'           => $model->link,
+                'lastmod'       => Sitemap::lastmodFormat($model->updated_at),
                 'changefreq'    => Sitemap::CHANGEFREQ_MONTHLY,
                 'priority'      => Sitemap::PRIORITY_60,
             );
